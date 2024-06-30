3e710d1e50c33c47bca09e7d95f0c178395eebf9
Risshi25 <risshirajsen@gmail.com>
2024-07-01 02:27:55 +0530

    added cors and code structure

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const socketio = require('socket.io');
 const mongoose = require("mongoose");
 const cors = require('cors');
 
-app.use(cors({ origin: 'http://localhost:3000' })); 
-
-const expressServer = app.listen(3001);
-const io = socketio(expressServer);
-
-const Game = require('./Models/Game')
+// Models
+const Game = require('./Models/Game');
 const randomTextApi = require('./randomTextApi');
 
+// CORS setup
+app.use(cors({ origin: 'http://localhost:3000' }));
 
+// Create HTTP server
+const server = http.createServer(app);
+
+// Set up Socket.io with CORS
+const io = socketio(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST"]
+  }
+});
+
+// MongoDB connection
 mongoose.connect('mongodb://localhost:27017/typeracerDB')
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error(err));
 
-io.on('connect',(socket)=>{
-  socket.emit("test","this is from the server")
-})
+// Socket.io connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.emit("test", "this is from the server");
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
